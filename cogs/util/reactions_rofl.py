d21987640a669c9f64fd05e7971cd71efaf9200a
ripkos <mposkrypko@gmail.com>
2020-12-07 09:53:57 +0100

    gg try2

--- cogs/util/reactions_rofl.py
+++ cogs/util/reactions_rofl.py
@@ -1,6 +1,10 @@
 import cogs.sqvrofl
+from cogs.util.jediutil import trash
 
 
 async def pepper(payload, bot):
-    channel = bot.get_channel(payload.channel_id)
-    await cogs.sqvrofl.sqvrofl.gg(channel)
+    ctx = bot.get_channel(payload.channel_id)
+    tosend = trash()
+    await ctx.send(tosend)
+    msg = await ctx.send("Next? 🌶️")
+    await msg.add_reaction('🌶️')
